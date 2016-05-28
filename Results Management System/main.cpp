2f9cf5f10ce6b493ab17867bef52936806f957fa
Wenting Ye <Wenting_Ye@Outlook.com>
2016-05-28 15:35:35 +0800

    Initial Commit

---
+++ Results Management System/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Results Management System
+//
+//  Created by YeWenting. on 16/5/28.
+//  Copyright © 2016年 School of Computer Science. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
